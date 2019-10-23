57cf7b46167af899176a3d8d6774fd43be49d323
nrovcanin1 <nrovcanin1@etf.unsa.ba>
2019-10-24 00:29:57 +0200

    Initial commit

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,41 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl [] ar = new Artikl[50];
+    int br = 0;
+
+    public boolean dodajArtikl(Artikl a)
+    {
+        if(br >= 50) return false;
+        ar[br] = new Artikl(a.getNaziv(), a.getCijena(), a.getKod());
+        br++;
+        return true;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod)
+    {
+        for(int i = 0; i < br; i++)
+        {
+            if(kod.equals(ar[i].getKod()))
+            {
+                Artikl rez = ar[i];
+                for(int j = i; j < br - 1; j++)
+                    ar[j] = ar[j + 1];
+                ar[br - 1] = null;
+                br--;
+                return rez;
+            }
+        }
+        return null;
+    }
+
+    public Artikl [] getArtikli() { return ar; }
+
+    public int dajUkupnuCijenuArtikala()
+    {
+        int s = 0;
+        for(int i = 0; i < br; i++)
+            s += ar[i].getCijena();
+        return s;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,35 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl [] ar = new Artikl[1000];
+    int br = 0;
+
+    public boolean dodajArtikl(Artikl a)
+    {
+        if(br >= 1000) return false;
+        ar[br] = new Artikl(a.getNaziv(), a.getCijena(), a.getKod());
+        br++;
+        return true;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod)
+    {
+        for(int i = 0; i < br; i++)
+        {
+            if(kod.equals(ar[i].getKod()))
+            {
+                Artikl a = ar[i];
+                for(int j = i; j < br - 1; j++)
+                    ar[j] = ar[j + 1];
+                ar[br - 1] = null;
+                br--;
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public Artikl [] getArtikli() { return ar; }
+
+
 }
